3da3d433d812bd3a2d38d1e7c4636f0f8c81e2c4
consultingninja <consultingninja.tech@gmail.com>
2023-05-21 14:13:54 -0500

    init auth

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,28 @@
+
+
+
+<nav>
+    <a href="/">Home</a>
+    <a href="/signup">Sign Up</a>
+    <a href="/read">Login</a>
+
+</nav>
+
+
+<style>
+    nav{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+    }
+    nav a{
+        margin: .5em;
+        color: #000000;
+        text-decoration: none;
+        font-weight: 800;
+    }
+    nav a:hover{
+        text-decoration: underline;
+    }
+</style>
